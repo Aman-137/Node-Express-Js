8f63a9f94363fdd22a0e835b7974dd5844fc7096
Aman Kumar <kumaramansingh137@gmail.com>
2022-03-14 16:15:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+// superheroes package
+// supervillains package.
+
+var superheroes = require("superheroes");
+var  supervillains = require("supervillains");
+
+var mySuperHeroName = superheroes.random();
+var mySuperVillainName = supervillains.random();
+
+console.log(mySuperHeroName);
+console.log(mySuperVillainName);

58bf386fbd7b2919df91a2823c766bb56ae60d99
Aman Kumar <kumaramansingh137@gmail.com>
2022-03-28 17:22:52 +0530

    Add files via upload

--- ExpressJs/WeatherProject/app.js
+++ ExpressJs/WeatherProject/app.js
@@ -15,13 +15,16 @@ app.get("/", function(req, res) {
     response.on("data", function(data) {
       const weatherData = JSON.parse(data);
       const temp = weatherData.main.temp;
-      const description = weatherData.weather[0].description;
-      console.log(temp);
-      console.log(description);
+      const weatherDescription = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+      res.write("<p>The weather is currently " + weatherDescription + "<p>");
+      res.write("<h1>The temperature in London is " + temp + " degree Celcius.</h1>")
+      res.write("<img src=" + imageURL + ">");
+      res.send();
     })
   })
 
-  res.send("Server is up and running.")
 })
 
 

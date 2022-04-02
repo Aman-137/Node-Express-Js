6bfd7c544c1f00aca572d776659c37aec96e1c08
Aman Kumar <kumaramansingh137@gmail.com>
2022-04-02 16:07:24 +0530

    Add files via upload

--- ExpressJs/WeatherProject/app.js
+++ ExpressJs/WeatherProject/app.js
@@ -1,13 +1,25 @@
 
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
 
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=b58fe93f37ef74f7acb0f1c8166226b6&units=metric"
+  res.sendFile(__dirname + "/index.html");
+
+});
+
+app.post("/", function(req, res) {
+
+  const querry = req.body.cityName;
+  const apiKey = "b58fe93f37ef74f7acb0f1c8166226b6";
+  const unit = "metric";
+
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + querry + "&appid=" + apiKey + "&units=" + unit + "";
 
   https.get(url, function(response) {
     console.log(response.statusCode);
@@ -19,14 +31,14 @@ app.get("/", function(req, res) {
       const icon = weatherData.weather[0].icon;
       const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
       res.write("<p>The weather is currently " + weatherDescription + "<p>");
-      res.write("<h1>The temperature in London is " + temp + " degree Celcius.</h1>")
+      res.write("<h1>The temperature in " + querry + " is " + temp + " degree Celcius.</h1>")
       res.write("<img src=" + imageURL + ">");
       res.send();
     })
   })
 
-})
 
+})
 
 
 

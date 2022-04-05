26ecea2607a1509d022f72020e57d50dea7cb792
Aman Kumar <kumaramansingh137@gmail.com>
2022-04-05 12:16:07 +0530

    Add files via upload

--- ExpressJs/Newsletter Signup/app.js
+++ ExpressJs/Newsletter Signup/app.js
@@ -2,6 +2,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
+
 
 
 const app = express();
@@ -17,15 +19,53 @@ app.get("/", function(req, res) {
 
 app.post("/", function(req, res) {
 
-  var firstName = req.body.fName;
-  var lastName = req.body.lName;
-  var email = req.body.email;
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
 
-  console.log(firstName, lastName, email);
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
 
-})
+  const  jsonData = JSON.stringify(data)
+
+  const url = "https://us14.api.mailchimp.com/3.0/lists/84d5c9ddd5";
+
+  const options = {
+    method: "POST",
+    auth: "aman1:87ec5ba8a03b51a67aa59380740a6256-us14"
+  }
+
+  const request = https.request(url, options, function(response){
+    request.on("data", function(data){
+      console.log(JSON.parse(data));
+    })
+  })
+
+  request.write(jsonData);
+  request.end();
+
+});
 
 
 app.listen(3000, function() {
   console.log("Server is running on port 3000.");
 });
+
+
+
+
+// API Key
+// 87ec5ba8a03b51a67aa59380740a6256-us14
+
+// List id
+// 84d5c9ddd5

5611f3c0dcd0b0a6fe429ea0cec240a1698f8a19
Aman Kumar <kumaramansingh137@gmail.com>
2022-04-16 17:31:53 +0530

    Add files via upload

--- ToDoList/app.js
+++ ToDoList/app.js
@@ -4,10 +4,12 @@ const bodyParser = require("body-parser");
 const app = express();
 
 let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res) {
 
@@ -25,27 +27,39 @@ app.get("/", function(req, res) {
 
 
 
-  res.render("list", {
-    kindOfDay: day,
-    newListItems: items
-  });
+  res.render("list", {listTitle: day, newListItems: items});
 
 });
 
 
 app.post("/", function(req, res) {
+
   let item = req.body.newItem;
 
-  items.push(item);
-
-  res.redirect("/");
+  if (req.body.list === "Work"){
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
 
 });
 
 
+app.get("/work", function(req, res) {
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
 
+app.get("/about", function(req, res) {
+  res.render("about");
+});
 
-
+app.post("/work", function(req, res) {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+});
 
 
 app.listen(3000, function() {
